405017625a640eab2f5053d4c0afa10920885944
Brent-Stockton <48369991+Brent-Stockton@users.noreply.github.com>
2022-12-21 23:05:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+//team Javascript title for cards
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
+
+
+
+//bio button Show/Hide
+function ShowAndHide() {
+  var x = document.getElementById('SectionName');
+  if (x.style.display == 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+document.getElementById('m-button').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
+
+document.getElementById('k-button').addEventListener("click", function() {
+	document.querySelector('.bg-modal-ken').style.display = "flex";
+});
+
+document.querySelector('.k-close').addEventListener("click", function() {
+	document.querySelector('.bg-modal-ken').style.display = "none";
+});
+
+document.getElementById('l-button').addEventListener("click", function() {
+	document.querySelector('.bg-modal-ken').style.display = "flex";
+});
